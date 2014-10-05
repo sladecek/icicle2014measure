28661d2a878bd81ea4c1214878769a9677aa487c
Ladislav Sladecek <ladislav.sladecek@gmail.com>
2014-10-05 21:15:49 +0200

    wip

--- iciview.cpp
+++ iciview.cpp
@@ -2,6 +2,8 @@
 #include "opencv2/highgui/highgui.hpp"
 #include <iostream>
 #include <fstream>
+#include <algorithm> 
+#include <cmath> 
 #include "dirent.h"
 
 using namespace std;
@@ -18,6 +20,43 @@ vector<int> times;
 vector<bool> outliers;
 vector<double> sums;
 vector<bool> accepted;
+Mat backgroundMean;
+Mat backgroundSd;
+Mat transformed;
+
+double transformPixel(int y, int x, uchar r, uchar g, uchar b)
+{
+    const double coef[] = { 1.0, 0.5, 1.0 };
+    const double p[] = { r, g, b };
+
+    double result = 0;
+    for (int i = 0; i < 3; i++)
+    {
+	double p0 = backgroundMean.at<Vec3b>(y, x)[i];
+	result += (p[i] - p0) * coef[i];
+    }
+    return result;
+}
+// --------------------------------------------------------------------------------
+
+void transformMatrix(const Mat& input, Mat* output)
+{
+    Size s = input.size();
+    *output = Mat(s, CV_8UC1);
+    for (int y = 0; y < s.height; y++) {
+	for (int x = 0; x < s.width; x++) {
+	    Vec3b intensity = input.at<Vec3b>(y, x);
+	    uchar blue = intensity.val[0];
+	    uchar green = intensity.val[1];
+	    uchar red = intensity.val[2];
+	    double d = transformPixel(y, x, red, green, blue);
+	    d = std::max(0.0, d);
+	    d = std::min(255.0, d);
+	    output->at<uchar>(y, x) = (uchar)floor(d); 		
+	}
+    }
+}
+// --------------------------------------------------------------------------------
 
 void saveTimeFunctionForOctave(int y, int x)
 {
@@ -44,12 +83,35 @@ void saveTimeFunctionForOctave(int y, int x)
 }
 // --------------------------------------------------------------------------------
 
+void saveTransformedTimeFunctionForOctave(int y, int x)
+{
+    ofstream octaveFile;
+    octaveFile.open ("oit.txt");
+    
+    for (int i = 0; i < images.size(); i++) 
+    {
+	if (accepted[i]) 
+	{
+	    Vec3b intensity = images[i].at<Vec3b>(y, x);
+	    uchar blue = intensity.val[0];
+	    uchar green = intensity.val[1];
+	    uchar red = intensity.val[2];
+	
+	    octaveFile << times[i] 
+		       << "," << transformPixel(y, x, red, green, blue)
+		       << endl;
+	}
+    }
+    octaveFile.close();
+}
+// --------------------------------------------------------------------------------
+
+
 void mouseClickCallback(int event, int x, int y, int flags, void* userdata)
 {
      if(event == EVENT_LBUTTONDOWN )
-     {
-	 
-	 saveTimeFunctionForOctave(y, x);	 
+     {	 
+	 saveTransformedTimeFunctionForOctave(y, x);	 
      }
 }
 // --------------------------------------------------------------------------------
@@ -90,7 +152,10 @@ void loadFiles(const string& dir)
       times.push_back(n - startTime);
 
       string fn = dir + "/" + files[i];
-      images.push_back(imread(fn));
+      Mat wholePicture(imread(fn));
+      Mat partial;
+      Mat(wholePicture, Range::all(), Range(90,320)).copyTo(partial);
+      images.push_back(partial);
       if (images.back().empty()) 
       {
 	  cerr << "unreadable image " <<  fn << endl;
@@ -121,7 +186,7 @@ void computeImageSums()
 }
 // --------------------------------------------------------------------------------
 
-void computeMeanAndDeviation(double* mean, double* sd)
+void computeImageMeanAndDeviation(double* mean, double* sd)
 {
     *mean = 0;
     *sd = 0;
@@ -157,7 +222,7 @@ void computeOutliers(double tolerance)
     int remain = 0;
     computeImageSums();
 
-    computeMeanAndDeviation(&mean, &sd);
+    computeImageMeanAndDeviation(&mean, &sd);
     if (sd != 0) {
 	int cnt = files.size();
 	for (int i = 0; i < cnt; i++) 
@@ -175,6 +240,13 @@ void computeOutliers(double tolerance)
     cout << "remain " << remain << " of " << files.size() << endl;
 }
 // --------------------------------------------------------------------------------
+
+void computeBackground()
+{
+
+}
+// --------------------------------------------------------------------------------
+
 
 
 Mat* findLastGood()
@@ -200,13 +272,15 @@ int main(int argc, char** argv)
   scanDirectory(argv[1]);
   loadFiles(argv[1]);
   computeOutliers(2.0);
+  computeBackground();
   Mat* lastGood = findLastGood();
+  transformMatrix(*lastGood, &transformed);
   if (lastGood != 0) 
   {
       const char* windowTitle = "iciview";
       namedWindow(windowTitle, 1);
       setMouseCallback(windowTitle, mouseClickCallback, NULL);
-      imshow(windowTitle, *lastGood);
+      imshow(windowTitle, transformed);
       waitKey(0);
   }
   
