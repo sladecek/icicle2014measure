fa11088b5cd14c158e638495e18f03ceeedd7118
Ladislav Sladecek <ladislav.sladecek@gmail.com>
2015-01-10 22:29:39 +0100

    wip

--- ex541.m
+++ ex541.m
@@ -25,7 +25,8 @@ penalty0 = mean(mean(www(:,1:3)));
 ww = min(2*penalty0,www);
 penalty = mean(mean(ww));
 w = ww - penalty;
-lambda = 1 * sx*sy*st;
+lambda = 0 * sx*sy*st;
+kappa = 0*sx*sy*st;
 
 theta0 = [0 1 0 0]';
 
@@ -43,7 +44,7 @@ wttt = w .* tttix;
 
 
 % --------------------------------------------------------------------------------------
-function [J, grad] = costFunction(theta, lambda,  tax, sy, st, v, w, wt, wtt, wttt)
+function [J, grad] = costFunction(theta, lambda,  kappa, tax, sy, st, v, w, wt, wtt, wttt)
 J = 0;
 grad = zeros(size(theta));
 l=min(1,max(0,tax * theta));
@@ -58,21 +59,23 @@ g1(2) = -2*sum(wt(lix))*sy;
 g1(3) = -2*sum(wtt(lix))*sy;
 g1(4) = -2*sum(wttt(lix))*sy;
 
-
 temp = theta;
 temp(1) = 0;
 J2 = lambda / (2 * 4) * temp'*temp;
 g2 = lambda * temp / 4;
 
-J = J1 + J2;
-grad = g1 + g2;
+J3 = kappa * ( theta(1)^2 + (sum(theta) - 1)^2);
+g3 = 2 * kappa * (theta + [theta(1),0,0,0]');
+
+J = J1 + J2 + J3;
+grad = g1 + g2 + g3;
 
 endfunction
 % --------------------------------------------------------------------------------------
 
 if true
 options = optimset('GradObj', 'on', 'MaxIter', 50);
-[theta] = fminunc (@(t)(costFunction(t, lambda,  tax, sy, st, v, w, wt, wtt, wttt)), theta0, options);
+[theta] = fminunc (@(t)(costFunction(t, lambda, kappa,  tax, sy, st, v, w, wt, wtt, wttt)), theta0, options);
 theta
 plot(tax*theta)
 endif
@@ -86,7 +89,7 @@ pl=zeros(nn0);
 for t0=1:nn0
    theta = zeros(4,1);
    theta(1) = th0(t0);
-   pl(t0) = costFunction(theta, lambda,  tax, sy, st, v, w, wt, wtt, wttt);
+   pl(t0) = costFunction(theta, lambda, kappa,  tax, sy, st, v, w, wt, wtt, wttt);
 endfor
 plot(th0, pl);
 ylabel("theta_0");
@@ -107,7 +110,7 @@ for t0=1:nn0
    theta = zeros(4,1);
    theta(1) = th0(t0);
    theta(2) = th1(t1);
-   pl(t0,t1) = costFunction(theta, lambda,  tax, sy, st, v, w, wt, wtt, wttt);
+   pl(t0,t1) = costFunction(theta, lambda, kappa,  tax, sy, st, v, w, wt, wtt, wttt);
 endfor
 endfor
 meshc(th0, th1, pl);
@@ -123,14 +126,14 @@ if false
 th0 = [0 1 0 0]';
 delta = 0.01
 
-[J,g] = costFunction(th0, lambda,  tax, sy, st, v, w, wt, wtt, wttt);
+[J,g] = costFunction(th0, lambda, kappa,  tax, sy, st, v, w, wt, wtt, wttt);
 for i=1:4
  th = th0;
  th(i) = th(i) + delta;
- Jplus = costFunction(th, lambda,  tax, sy, st, v, w, wt, wtt, wttt)
+ Jplus = costFunction(th, lambda,  kappa, tax, sy, st, v, w, wt, wtt, wttt)
  th = th0;
  th(i) = th(i) - delta;
- Jminus = costFunction(th, lambda,  tax, sy, st, v, w, wt, wtt, wttt)
+ Jminus = costFunction(th, lambda,  kappa, tax, sy, st, v, w, wt, wtt, wttt)
  g2 = (Jplus - Jminus) / (2*delta)
  g(i)
 endfor
